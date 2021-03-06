271f9f9edd8b44a772705401df00f6e0b355145b
Linus Långberg <git@phoqe.dev>
2021-03-06 19:17:26 +0100

    feat: add remmi

---
+++ Casks/remmi.rb
@@ -0,0 +1,11 @@
+cask "remmi" do
+  version "1.6"
+  sha256 "3942c6d0b0978f5ca72a788cdd63a94d8f12392328ae03e0aeaed3f495e26aa4"
+
+  url "https://github.com/phoqe/remmi/releases/download/#{version}/Remmi.zip"
+  name "Remmi"
+  desc "Daily GitHub contributions in your macOS status bar."
+  homepage "https://github.com/phoqe/remmi"
+
+  app "Remmi.app"
+end
